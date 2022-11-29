38052e2f46ef774fc61df47b55fd58173ba4258e
Dylan <dhalpern@uchicago.edu>
2022-11-29 16:32:57 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
   experimental: {
     appDir: true,
-    runtime: "experimental-edge",
   },
   baseUrl: ".",
   paths: {
     "@/utils": ["./utils"],
-    "@/components": ["./components"],
+    "@/components": ["./components"]
   },
 };
 
