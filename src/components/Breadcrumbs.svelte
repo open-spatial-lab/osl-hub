38687670e688a0b7fa1bcb7a5448c59016951661
Dylan <dhalpern@uchicago.edu>
2022-12-15 12:14:38 -0600

    Styling and UI

---
+++ src/components/Breadcrumbs.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { ParentBreadcrumbSpec } from 'src/utils/NotionClient';
+	import { Breadcrumb, Crumb } from '@skeletonlabs/skeleton';
+	export let steps: ParentBreadcrumbSpec[];
+	export let showHome: boolean = true;
+</script>
+
+<Breadcrumb class="pt-4">
+	{#if showHome}
+		<Crumb href="/">
+			<svelte:fragment slot="lead">H</svelte:fragment>
+		</Crumb>
+	{/if}
+	{#each steps as step}
+		<Crumb href={`/${step.type}/${step.id}`}>{step.name}</Crumb>
+	{/each}
+</Breadcrumb>
---
+++ src/components/NotionRelations/NotionRelations.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	import { writable, type Writable } from 'svelte/store';
+	import NotionRelationCard from '../NotionRelationCard';
+	const storeTab: Writable<string> = writable('a');
+	export let properties: { name: string; relations: { id: string }[] }[];
+</script>
+
+<hr />
+<h3>Connections</h3>
+<TabGroup selected={storeTab}>
+    {#each properties as property, i}
+        <Tab tab={property.name}>
+            {property.name}
+        </Tab>
+    {/each}
+</TabGroup>
+
+<!-- Conditionally display content -->
+<!-- {#if $storeTab === 'a'}Content A{/if}
+{#if $storeTab === 'b'}Content B{/if}
+<TabContent vertical class="py-2">
+    {#each properties as property, i}
+        <TabPane tabId={property.name} tab={property.name} active={i===0}>
+            {#each property.relations as relation}
+                <NotionRelationCard id={relation.id} />
+            {/each}
+        </TabPane>
+    {/each}
+</TabContent>
+<hr/> -->
