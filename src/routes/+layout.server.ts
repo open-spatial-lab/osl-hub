a915fe3195ace8041dbba3241daf05c7146db3e1
Dylan <dhalpern@uchicago.edu>
2022-12-16 12:06:41 -0600

    Update +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,7 @@
 import type { LayoutServerLoad } from "./$types"
 
 export const load: LayoutServerLoad = async ({ locals }) => {
+  console.log("layout server load", await locals.getSession())
   return {
     session: await locals.getSession(),
   }
