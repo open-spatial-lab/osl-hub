b82994349b235d03f4e4c9c704f15e5927ab198a
Dylan <dhalpern@uchicago.edu>
2022-12-16 14:36:56 -0600

    Ancillary pages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,14 @@
-<div>Contact</div>
+<script lang="ts">
+    import { GradientHeading } from '@skeletonlabs/skeleton';
+</script>
+
+<div
+  class="max-w-screen-2xl m-auto flex flex-col items-start p:8 space-y-8 lg:p-0"
+>
+  <GradientHeading tag="h1" class="text-8xl" direction="bg-gradient-to-r" from="from-primary-500" to="to-accent-500">
+    Contact
+  </GradientHeading>
+  <p>
+    Get in touch by email openspatial [at] uchicago [dot] edu!
+  </p>
+</div>
