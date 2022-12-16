a2e2702ec99083d8a2bdd3b3effea9b97beabd35
Dylan <dhalpern@uchicago.edu>
2022-12-16 11:35:55 -0600

    Clean up for auth issue

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,23 +1,9 @@
+
 import SvelteKitAuth from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import Google from "@auth/core/providers/google"
-import { 
-  SECRET_GITHUB_CLIENT_ID,
-  SECRET_GITHUB_CLIENT_SECRET,
-  SECRET_GOOGLE_CLIENT_ID,
-  SECRET_GOOGLE_CLIENT_SECRET,
-} from "$env/static/private"
+import { SECRET_GITHUB_CLIENT_ID, SECRET_GITHUB_CLIENT_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
-  // trustHost: true,
-  providers: [
-    GitHub({ 
-      clientId: SECRET_GITHUB_CLIENT_ID,
-      clientSecret: SECRET_GITHUB_CLIENT_SECRET
-    }),
-    // Google({
-    //   clientId: SECRET_GOOGLE_CLIENT_ID,
-    //   clientSecret: SECRET_GOOGLE_CLIENT_SECRET
-    // })
-  ],
+  trustHost: true,
+  providers: [GitHub({ clientId: SECRET_GITHUB_CLIENT_ID, clientSecret: SECRET_GITHUB_CLIENT_SECRET })],
 })
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite"
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-};
+  plugins: [sveltekit()],
+}
 
-export default config;
+export default config
