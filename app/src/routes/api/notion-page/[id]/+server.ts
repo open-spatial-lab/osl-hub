44d9c2df969d37d0034bc23e6a94d02106c936d5
Dylan <dhalpern@uchicago.edu>
2022-12-21 10:41:00 -0600

    Revert "Revert "Fixes and cleanup""
    This reverts commit 5fa966ce1ef7693e6e8b9a5337f53c878f478e98.

--- app/src/routes/api/notion-page/[id]/+server.ts
+++ app/src/routes/api/notion-page/[id]/+server.ts
@@ -1,6 +1,5 @@
 import type { RequestHandler } from './$types';
-import { error } from '@sveltejs/kit';
-import notion, { type ServerGetPageContentResponse } from '../../../../lib/utils/NotionClient';
+import notion from '../../../../lib/utils/NotionClient';
 
 /** @type {import('./$types').RequestHandler} */
 export const GET: RequestHandler = async ({params}) => {
