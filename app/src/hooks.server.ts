677241fa528472a304919b54f3bca42c79a5a526
Dylan <dhalpern@uchicago.edu>
2023-01-04 15:46:09 -0600

    Add google login provider

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -1,8 +1,12 @@
 import {SvelteKitAuth} from "@auth/sveltekit";
 import GitHub from "@auth/core/providers/github";
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
+import Google from "@auth/core/providers/google";
+import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET  } from "$env/static/private";
 
 export const handle = SvelteKitAuth({
   trustHost: true,
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
+  ],
 });
